c2b5d11d1153c77c2f2f33cf78bf99de39061583
karim242 <karimelsa38@gmail.com>
2024-05-06 23:38:59 +0300

    delayed future to navigate to home view

--- lib/Features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,7 +1,11 @@
+import 'package:bookly/Features/home/presentation/view/home_view.dart';
 import 'package:bookly/Features/splash/presentation/view/widgets/sliding_image.dart';
 import 'package:bookly/Features/splash/presentation/view/widgets/sliding_text.dart';
+import 'package:bookly/constance.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -18,6 +22,19 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTranstionDuration);
+    });
+  }
+
+  void initSlidingAnimation() {
     animationController =
         AnimationController(vsync: this, duration: const Duration(seconds: 1));
 
