53af286261a15b90945e0f1b2b97e85cd5429bd8
karim242 <karimelsa38@gmail.com>
2024-05-06 23:07:33 +0300

    add animation to text and logo to splash screen

---
+++ lib/Features/splash/presentation/view/widgets/sliding_image.dart
@@ -0,0 +1,20 @@
+
+import 'package:bookly/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+
+
+class SlidingLogoImage extends StatelessWidget {
+  const SlidingLogoImage({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedCrossFade(
+      firstChild: const SizedBox(),
+      secondChild: Center(child: Image.asset(AssetsData.logo)),
+      crossFadeState: CrossFadeState.showSecond,
+      duration: const Duration(seconds: 1),
+    );
+  }
+}
--- lib/Features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,8 +1,38 @@
-import 'package:bookly/core/utils/assets_data.dart';
+import 'package:bookly/Features/splash/presentation/view/widgets/sliding_image.dart';
+import 'package:bookly/Features/splash/presentation/view/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +40,9 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
-        const Text(
-          'Read Books For Free ',
-          textAlign: TextAlign.center,
-        )
+        const SlidingLogoImage(),
+        const SizedBox(height: 15),
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
